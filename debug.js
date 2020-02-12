c44de2c1a18608eed3d3c842d819b87ee44b7461
Zac <zacace@ingoglia.com>
2020-02-12 15:12:19 -0700

    Pure logical Errors remain
    Everything but logic errors corrected

--- debug.js
+++ debug.js
@@ -1,58 +1,64 @@
+    //function for test 1
+function test1() {
+    var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>";
+    var output = document.getElementById("output");
 
-//function for test
-function test1(){
-    var inPut = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
-    var output = document.getElementById(ouput);
-    
     //Creates and initialized all of the variables for the program
-    var pos = Array ();
+    var pos = new Array();
     var x = 0;
-    var y === 0;    
+    var y = 0;
     var ans = 0;
 
     //creates a grid array with one element "0,0"
     var grid = [0, 0];
-    //splits everything in the grid array 
-    grid = inpuT.split(" ");
+    //splits everything in the grid array
+    grid = input.split("");
 
-    pos..push(x + ", " ++ y);
+    pos.push(x + ", " + y); //starting coord
+    console.log({ "pos": pos, "grid": grid });
 
-    //checks each grid element for the symbol in it and then moves accordingly 
-    for(var i = 0; i < grid.length; i++){
-        check = false;
-        if(grid(i) === "^"){
+    //checks each grid element for the symbol in it and then moves accordingly
+    for (var i = 0; i < grid.length; i++) {
+        // ch   eck = false;
+        if (grid[i] === "^") {
             x++;
-        }else (grid[i] === ">"){
+        }
+        else if (grid[i] === ">") {
             y++;
-        }else if(grid[i] = "v"){
+        }
+        else if (grid[i] === "v") {
             x--;
-        }else if(grid[i] === <){
+        }
+        else if (grid[i] === "<") {
             y--;
         }
         pos.push(x + ", " + y);
-    
+        // console.log(x + ", " + y);
 
-    //loop that will make sure no position is counted twice 
-    loop:
-    for(var j = 0; j<pos.length; j++){
-        for(var i = j+1; i<pos.length-1; i++){
-            if(pos[i] === pos[j]){
-                continue loop;
+
+        //loop that will make sure no position is counted twice
+        loop:
+        for (var j = 0; j < pos.length; j++) {
+            for (var k = j + 1; k < pos.length - 1; k++) {
+                if (pos[k] === pos[j]) {
+                    continue loop;
+                }
             }
+            ans++;
         }
-        ans++;
     }
-    output.textContent.anchor.innerHTML = Ans;
+    output.innerHTML = ans;
+}
 
 //function for test 2
-test(){
-    var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
+function test2() {
+    var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>";
     var output = document.getElementById("output");
-    var poS = Array [];
-    var x1 === 0;
-    var y1 = 0;   
-    var X2 = 0;
-    var y2 = 0;  
+    var pos = new Array();
+    var x1 = 0;
+    var y1 = 0;
+    var x2 = 0;
+    var y2 = 0;
     var ans = 0;
 
     //grid array that store all of the positions that were visited
@@ -62,43 +68,51 @@ test(){
     pos.push("0, 0");
 
     //checks all the even values
-    for(var i = 0; i < grid.length; i++){
+    for (var i = 0; i < grid.length; i++) {
         check = false;
-        if(i % 2 === 0){
-            if(grid[i] === "^"){
+        if (i % 2 === 0) {
+            if (grid[i] === "^") {
                 x1++;
-            }else if(grid[i] !=== ">"){
+            }
+            else if (grid[i] !== ">") {
                 y1++;
-            }else if(grid(i)) === "v"){
+            }
+            else if (grid[i] === "v") {
                 x1--;
-            }else if(grid[i] === "<"){
+            }
+            else if (grid[i] === "<") {
                 y1--;
             }
             pos.push(x1 + ", " + y1);
-        
-        //checks all of the odd values
-        else if(i % 2 === 1){
-            if(grid[i] === "^"){
-                x2++;
-            }else if(gRid === ">"){
-                y2++;
-            }elseif(rid[i] === "v"){
-                x2--;
-            }else if(grid[I] === "<"){
-                y2--;
-            }
-            pos push(x2 + ", " + y2);
-        }
-    }
 
-    //loop to check repeats
-    loose:
-    for(var j = 0; j<poS.lngth; j++){
-        for(var i = j+1; i<pos.length()-1; i++){
-            if(pos[i] === pos[j]){
-                continue loop;
+            //checks all of the odd values
+            if (i % 2 === 1) {
+                if (grid[i] === "^") {
+                    x2++;
+                }
+                else if (grid[i] === ">") {
+                    y2++;
+                }
+                else if (grid[i] === "v") {
+                    x2--;
+                }
+                else if (grid[i] === "<") {
+                    y2--;
+                }
+                pos.push(x2 + ", " + y2);
             }
         }
-        ans++;
+
+        //loop to check repeats
+        loop:
+        for (var j = 0; j < pos.length; j++) {
+            for (var i = j + 1; i < pos.length - 1; i++) {
+                if (pos[i] === pos[j]) {
+                    continue loop;
+                }
+            }
+            ans++;
+        }
     }
-    output.document.innerHTMl = ans;
+    output.innerHTML = ans;
+}
